005b3c5ec0ab9fac8e1ea93ba35b78e2cfdf33f4
Oleksandr Horobiy <izzwerg.kr@gmail.com>
2025-09-06 12:08:04 +0300

    some stuff

--- src/header.js
+++ src/header.js
@@ -1,11 +1,18 @@
-const header = document.querySelector('.head');
+// const header = document.querySelector('.head');
 // const main = document.querySelector('.main');
-window.onwheel = e => {
-  if(e.deltaY >= 0){
-    // Wheel Down
-    header.classList.add('sticky');
-  } else {
-    // Wheel Up
-    header.classList.remove('sticky');
-  }
+const headNavMobile = document.querySelector('.head-nav-mobile');
+
+// window.onwheel = e => {
+//   if(e.deltaY >= 0){
+//     // Wheel Down
+//     header.classList.add('sticky');
+//   } else {
+//     // Wheel Up
+//     header.classList.remove('sticky');
+//   }
+// }
+
+headNavMobile.onclick = () => {
+  // header.classList.toggle('sticky');
+  document.querySelector('.head-mob-menu').classList.toggle('open');
 }
