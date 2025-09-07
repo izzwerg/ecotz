29f7657f9cf3f8bd2bb100435faaef50ad4582ea
Oleksandr Horobiy <izzwerg.kr@gmail.com>
2025-09-07 17:21:30 +0300

    scroll

---
+++ src/main.js
@@ -0,0 +1,16 @@
+let isScrolling = false;
+
+window.addEventListener("wheel", (e) => {
+  if (isScrolling) return; 
+  isScrolling = true;
+
+  const direction = e.deltaY > 0 ? 1 : -1;
+  const nextPos = window.scrollY + direction * window.innerHeight;
+
+  window.scrollTo({
+    top: nextPos,
+    behavior: "smooth"
+  });
+
+  setTimeout(() => isScrolling = false, 300); // затримка для завершення анімації
+});
