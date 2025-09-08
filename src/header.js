8d9bb61b958120b74721395c96075230f64c53ca
Oleksandr Horobiy <izzwerg.kr@gmail.com>
2025-09-08 16:25:02 +0300

    some stuff

--- src/header.js
+++ src/header.js
@@ -4,3 +4,11 @@ headNavMobile.onclick = () => {
 
   document.querySelector('.head-mob-menu').classList.toggle('open');
 }
+
+const menuLinks = document.querySelectorAll('.menu-link');
+
+menuLinks.forEach(link => {
+  link.onclick = () => {
+    document.querySelector('.head-mob-menu').classList.remove('open');
+  }
+})
